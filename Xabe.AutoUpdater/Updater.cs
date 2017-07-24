f4df66c2c351a7fe1c4441ebe7eb209fd59bb562
tzmuda <tomasz.a.zmuda@gmail.com>
2017-07-24 21:35:19 +0200

    Initial commit

---
+++ Xabe.AutoUpdater/Updater.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using System.Net;
+
+namespace Xabe.AutoUpdater
+{
+    public class Updater
+    {
+        private readonly string _url;
+
+        public Updater(string url)
+        {
+            _url = url;
+        }
+
+        public void Get()
+        {
+            string fileName = Path.GetTempFileName();
+            using(var client = new WebClient())
+            {
+                client.DownloadFile(_url, fileName);
+            }
+
+            string tmpDir = Path.Combine(Path.GetTempPath(), Guid.NewGuid().ToString());
+            System.IO.Compression.ZipFile.ExtractToDirectory(fileName, tmpDir);
+        }
+    }
+}
