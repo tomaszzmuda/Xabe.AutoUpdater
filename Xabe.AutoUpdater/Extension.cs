1a8b4e18cac60eaf299de9ced29f5474cca321d0
Arnaud Dartois <nonobis@gmail.com>
2017-12-17 12:38:17 +0100

    Optimizing FtpProvider with author comment

---
+++ Xabe.AutoUpdater/Extension.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Xabe.AutoUpdater
+{
+    public static class Extension
+    {
+        public static bool Contains(this string source, string toCheck, StringComparison comp)
+        {
+            return source != null && toCheck != null && source.IndexOf(toCheck, comp) >= 0;
+        }
+    }
+}
